077703ce4c0851514aa8157feac6fa362e3b7ad2
Chaitany Arora <arorachaitany15@gmail.com>
2024-08-20 13:59:03 +0530

    made changes in yaml

---
+++ btest.py
@@ -0,0 +1,28 @@
+import subprocess
+import os
+
+
+cwd = os.path.dirname(os.path.abspath(__file__))
+
+blender_executable = os.environ.get("PATH")
+blend_file_paths = [os.path.join(cwd, 'test_input', 'test_drone.blend')]
+test_script_paths = [os.path.join(cwd, 'test_cases', 'test_drone.py')]
+
+def test():
+    for x in range(len(test_script_paths)):
+
+        blender_command = [blender_executable, blend_file_paths[x], '-P', test_script_paths[x]]
+
+        try:
+            #To read the console output
+            # result = subprocess.run(blender_command, stdout=subprocess.PIPE, text=True, check=True)
+            print(f'Started testing - {test_script_paths[x].rsplit("test_", 1)[-1]}')
+            # print(result.stdout)
+
+        except subprocess.CalledProcessError as e:
+            print("Error:", e)
+
+        print(f'Done testing - {test_script_paths[x].rsplit("test_", 1)[-1]}')
+
+if __name__ == '__main__':
+    test()
---
+++ test_drone.py
@@ -0,0 +1,53 @@
+import bpy
+from datetime import datetime
+import os
+
+
+# Iterate over all registered classes
+for cls in bpy.types.Panel.__subclasses__():
+    if 'Drone Setup (' in cls.bl_label: 
+        version = (cls.bl_label.split('(')[1][:-1])
+
+cwd = os.path.dirname(os.path.abspath(__file__))
+
+cwd = os.path.abspath(os.path.join(cwd, os.pardir, os.pardir))
+
+# Create a file 
+log_file = os.path.join(cwd, f'test_report_{version}.txt')
+
+# Panel: Formation
+
+# Section: Drone Setup
+
+# Part 1: (Inputs)
+bpy.context.scene.my_tool.drone.total_spheres = 20
+bpy.context.scene.my_tool.drone.total_rows = 10
+bpy.context.scene.my_tool.drone.x_diff = 1.00
+bpy.context.scene.my_tool.drone.y_diff = 1.00
+
+# Part 2: Sphere Grid
+bpy.ops.object.sphere_grid()
+
+# Test 1: (Sphere Grid) Check if spheres are added successfully
+if bpy.data.objects.get('1') or bpy.data.objects.get('1EL'):
+    timestamp = datetime.now().strftime("[%Y-%m-%d %H:%M:%S]")
+    with open(log_file, 'a') as file:
+        file.write(f'{timestamp} Test passed ✅ - sphere grid\n')
+else:
+    timestamp = datetime.now().strftime("[%Y-%m-%d %H:%M:%S]")
+    with open(log_file, 'a') as file:
+        file.write(f'{timestamp} Test failed ❌ - sphere grid\n')
+
+
+# Part 3: Reset
+bpy.ops.object.reset_scene()
+
+# Test 2: (Reset) Check if collection is empty or not
+if len(bpy.data.collections)==0:
+    timestamp = datetime.now().strftime("[%Y-%m-%d %H:%M:%S]")
+    with open(log_file, 'a') as file:
+        file.write(f'{timestamp} Test passed ✅ - reset\n')
+else:
+    timestamp = datetime.now().strftime("[%Y-%m-%d %H:%M:%S]")
+    with open(log_file, 'a') as file:
+        file.write(f'{timestamp} Test failed ❌ - reset\n')
