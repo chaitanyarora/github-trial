918107d19ce82fffa8a38641a2cc780242eb3b5f
Chaitany Arora <arorachaitany15@gmail.com>
2024-08-20 16:50:48 +0530

    Main: Install Plugin

--- install_plugin.py
+++ install_plugin.py
@@ -1,3 +1,7 @@
-import bpy
+import subprocess
 
-bpy.data.window_managers["WinMan"].addon_search = "drone"
+blender_executable = r"/home/runner/work/github-trial/github-trial/blender-4.2.0-linux-x64/blender"
+addon_script = "/home/runner/work/github-trial/github-trial/install_addon.py"
+
+# Call Blender with the script
+subprocess.run([blender_executable, '--background', '--python', addon_script])
