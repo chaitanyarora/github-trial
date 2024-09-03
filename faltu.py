04233e0e207cefdffa1d1327d0c8637a0a0bc705
Chaitany Arora <arorachaitany15@gmail.com>
2024-09-03 15:52:29 +0530

    Done

---
+++ faltu.py
@@ -0,0 +1,7 @@
+# blender-4.2.0-linux-x64/4.2/python/bin/pythyon3.11 -m pip install --target "blender-4.2.0-linux-x64/4.2/python/lib/python3.11/site-packages" scipy
+          
+#           /home/runner/work/github-trial/github-trial/blender-4.2.0-linux-x64/blender  --background --python-expr "
+#           import bpy
+
+#           bpy.ops.preferences.addon_install(filepath='/home/runner/work/github-trial/github-trial/animation_tool.zip')
+#           bpy.ops.preferences.addon_enable(module='animation_tool')"
